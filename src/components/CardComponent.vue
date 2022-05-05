0702f297c9cd62b3bf44829025d0f41b6b626863
muccidavide <davidemucci0804@gmail.com>
2022-05-05 18:45:58 +0200

    Add Mobile Version

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -1,49 +1,53 @@
 <template>
-    <div class="col text-center p-4">
+  <div class="col text-center p-4">
     <div class="card_album p-3">
-        <div class="img_album">
+      <div class="img_album">
         <img :src="album.poster" :alt="album.title" />
-        </div>
-        <h4 class="py-3">{{ album.title }}</h4>
-        <div class="info_album">
+      </div>
+      <h4 class="py-3">{{ album.title }}</h4>
+      <div class="info_album">
         <div class="author">
-            {{ album.author }}
+          {{ album.author }}
         </div>
         <div class="year">
-            {{ album.year }}
+          {{ album.year }}
         </div>
-        </div>
+      </div>
     </div>
-    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "CardComponent",
-        props: {
-            album: Object,
-        }
-    };
+export default {
+  name: "CardComponent",
+  props: {
+    album: Object,
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-.col{
-
-    height: 28rem;
-    .card_album {
-        min-height: 100%;
-        background-color: $ds-primary;
-        img{
-            aspect-ratio: 1/1;
-        }
-        h4 {
-            color: $ds-white;
-        }
-        .info_album {
-            color: $ds-light-dark;
-        }
+.col {
+  height: 28rem;
+  .card_album {
+    min-height: 100%;
+    background-color: $ds-primary;
+    img {
+      aspect-ratio: 1/1;
+      width: 100%;
     }
+    h4 {
+      color: $ds-white;
+    }
+    .info_album {
+      color: $ds-light-dark;
+    }
+  }
 }
 
-
+@media screen and (max-width: 600px) {
+  .col {
+    height: unset;
+  }
+}
 </style>
